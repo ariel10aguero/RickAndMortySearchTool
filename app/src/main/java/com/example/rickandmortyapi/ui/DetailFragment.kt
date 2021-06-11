fb8e9a902f78ce563dc99a607c83444951b4a1be
Alejandro <ariel10aguero@gmail.com>
2021-06-11 10:24:04 -0300

    Fragment details

--- app/src/main/java/com/example/rickandmortyapi/ui/DetailFragment.kt
+++ app/src/main/java/com/example/rickandmortyapi/ui/DetailFragment.kt
@@ -1,24 +1,19 @@
 package com.example.rickandmortyapi.ui
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.viewModels
-import androidx.fragment.app.viewModels
 import com.example.rickandmortyapi.R
+import com.example.rickandmortyapi.databinding.FragmentDetailBinding
 import com.example.rickandmortyapi.databinding.FragmentHomeBinding
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
-class HomeFragment : Fragment() {
 
-        private var _binding: FragmentHomeBinding? = null
-        private val binding get() = _binding!!
-    private val viewModel: MainViewModel by viewModels()
+class DetailFragment : Fragment() {
 
+    private var _binding: FragmentDetailBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,14 +23,10 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentDetailBinding.inflate(inflater, container, false)
         val view = binding.root
         return view
-    }
-
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
 
     }
+
 }
