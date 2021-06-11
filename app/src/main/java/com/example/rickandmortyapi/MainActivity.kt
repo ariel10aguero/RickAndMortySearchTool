2e1b02692f4f85215952c98c475b6ac78534911e
Alejandro <ariel10aguero@gmail.com>
2021-06-11 17:00:02 -0300

    observers in viewmodel

--- app/src/main/java/com/example/rickandmortyapi/MainActivity.kt
+++ app/src/main/java/com/example/rickandmortyapi/MainActivity.kt
@@ -26,66 +26,7 @@ class MainActivity : AppCompatActivity(){
         val view = binding.root
         setContentView(view)
 
-        viewModel.getLocation(type = "io")
-        viewModel.getEpisode(episode = "S04E10")
-
-        setUpCharacterObserver()
-        setUpLocationObserver()
-        setUpEpisodeObserver()
 
     }
-
-
-
-
-    private fun setUpCharacterObserver(){
-        viewModel.characterState.observe(this, Observer {characterState ->
-            when(characterState){
-                is DataState.Loading -> {
-                    Log.d("loading", "Loading Character")
-                }
-                is DataState.Success<CharacterResponse> -> {
-                    Log.d("character", "${characterState.data.results}")
-                }
-                is DataState.Error -> {
-                    Log.d("error", "${characterState.exception}")
-                }
-            }
-        })
-    }
-
-    private fun setUpLocationObserver(){
-        viewModel.locationState.observe(this, Observer {locationState ->
-            when(locationState){
-                is DataState.Loading -> {
-                    Log.d("loading Location", "Loading Location")
-
-                }
-                is DataState.Success<LocationResponse> -> {
-                    Log.d("location", "${locationState.data.results}")
-                }
-                is DataState.Error -> {
-                    Log.d("errorLocation", "${locationState.exception}")
-                }
-            }
-        })
-    }
-
-    private fun setUpEpisodeObserver(){
-        viewModel.episodeState.observe(this, Observer {episodeState ->
-            when(episodeState){
-                is DataState.Loading -> {
-                    Log.d("loading", "Loading Episode")
-                }
-                is DataState.Success<EpisodeResponse> -> {
-                    Log.d("episode", "${episodeState.data.results}")
-                }
-                is DataState.Error -> {
-                    Log.d("error", "${episodeState.exception}")
-                }
-            }
-        })
-    }
-
 
 }
--- app/src/main/java/com/example/rickandmortyapi/ui/HomeFragment.kt
+++ app/src/main/java/com/example/rickandmortyapi/ui/HomeFragment.kt
@@ -7,17 +7,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.rickandmortyapi.R
+import com.example.rickandmortyapi.data.model.CharacterResponse
+import com.example.rickandmortyapi.data.model.EpisodeResponse
+import com.example.rickandmortyapi.data.model.LocationResponse
 import com.example.rickandmortyapi.databinding.FragmentHomeBinding
+import com.example.rickandmortyapi.util.DataState
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
 
-        private var _binding: FragmentHomeBinding? = null
-        private val binding get() = _binding!!
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
     private val viewModel: MainViewModel by viewModels()
 
 
@@ -38,9 +43,68 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel.getLocation(type = "io")
+        viewModel.getEpisode(episode = "S04E09")
+
+        setUpCharacterObserver()
+        setUpLocationObserver()
+        setUpEpisodeObserver()
+
         binding.characterSearchBtn.setOnClickListener {
             findNavController().navigate(R.id.action_homeFragment_to_detailFragment)
         }
 
     }
+
+    private fun setUpCharacterObserver(){
+        viewModel.characterState.observe(viewLifecycleOwner, Observer {characterState ->
+            when(characterState){
+                is DataState.Loading -> {
+                    Log.d("loading", "Loading Character")
+                }
+                is DataState.Success<CharacterResponse> -> {
+                    Log.d("character", "${characterState.data.results}")
+                }
+                is DataState.Error -> {
+                    Log.d("error", "${characterState.exception}")
+                }
+            }
+        })
+    }
+
+    private fun setUpLocationObserver(){
+        viewModel.locationState.observe(viewLifecycleOwner, Observer {locationState ->
+            when(locationState){
+                is DataState.Loading -> {
+                    Log.d("loading Location", "Loading Location")
+
+                }
+                is DataState.Success<LocationResponse> -> {
+                    Log.d("location", "${locationState.data.results}")
+                }
+                is DataState.Error -> {
+                    Log.d("errorLocation", "${locationState.exception}")
+                }
+            }
+        })
+    }
+
+    private fun setUpEpisodeObserver(){
+        viewModel.episodeState.observe(viewLifecycleOwner, Observer {episodeState ->
+            when(episodeState){
+                is DataState.Loading -> {
+                    Log.d("loading", "Loading Episode")
+                }
+                is DataState.Success<EpisodeResponse> -> {
+                    Log.d("episode", "${episodeState.data.results}")
+                }
+                is DataState.Error -> {
+                    Log.d("error", "${episodeState.exception}")
+                }
+            }
+        })
+    }
+
+
+
 }
